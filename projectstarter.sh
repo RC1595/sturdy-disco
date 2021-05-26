0ec0805e2e64156b40259fe0796067920fe99725
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-05-25 21:17:29 -0600

    Started skeleton, added images and video to homepage

---
+++ projectstarter.sh
@@ -0,0 +1,19 @@
+#!usr/bin/bash
+mkdir Okotoks_Pizza;
+cd Okotoks_Pizza;
+touch index.html;
+echo "creating new project";
+echo "hello world"> ./index.html;
+mkdir css;
+cd css;
+touch style.css;
+cd ..;
+mkdir js;
+cd js;
+touch app.js;
+echo "All done!";
+exit;
+
+
+
+
